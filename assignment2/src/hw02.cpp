1c76253da75936b6d1e0f3e25022cdef9e77f499
zmocheng <zmocheng@gmail.com>
2021-11-24 21:47:37 +0100

    Change code in struct Raster, mainly in operator overloading: return pixels[y+x*max_x].

--- assignment2/src/hw02.cpp
+++ assignment2/src/hw02.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <cassert>
+#include <fstream>
 
 #include "gdal_priv.h"
 #include "cpl_conv.h"
@@ -21,6 +22,7 @@ struct Raster {
     // Fill values of an entire row
     void add_scanline(const int* line) {
         for (int i = 0; i < max_x; ++i) pixels.push_back(line[i]);
+        
     }
 
     // Fill entire raster with zeros
@@ -33,14 +35,14 @@ struct Raster {
     int& operator()(int x, int y) {
         assert(x >= 0 && x < max_x);
         assert(y >= 0 && y < max_y);
-        return pixels[x + y * max_x];
+        return pixels[y + x * max_x];
     }
 
     // Access the value of a raster cell to read it
     int operator()(int x, int y) const {
         assert(x >= 0 && x < max_x);
         assert(y >= 0 && y < max_y);
-        return pixels[x + y * max_x];
+        return pixels[y + x * max_x];
     }
 };
 
@@ -72,65 +74,134 @@ std::ostream& operator<<(std::ostream& os, const RasterCell& c) {
     return os;
 }
 
+// Write the raster file to an .asc file
+void output_raster(const Raster& raster, const double& pixelsize, const double& topx, const double& topy)
+{
+    int ncols(raster.max_x), nrows(raster.max_y);
+    double lowy = topy - pixelsize * raster.max_y;
+
+    std::ofstream outfile("D:/AlbertQ2/GEO1015/test.asc", std::ios::out); 
+    if (!outfile)std::cout << "File open issue, please check. " << '\n';
+    else
+    {
+        outfile << "NCOLS" << " " << ncols << '\n';
+        outfile << "NROWS" << " " << nrows << '\n';
+        outfile << "XLLCORNER" << " " << topx << '\n'; // low-left and up-left: x coordinate is same
+        outfile << "YLLCORNER" << " " << lowy << '\n'; // origin y is topy, need to be converted to lowy
+        outfile << "CELLSIZE" << " " << pixelsize << '\n';
+        outfile << "NODATA_VALUE" << " " << -9999 << '\n';
+
+        for (int i = 0; i < nrows; ++i)
+        {
+            for (int j = 0; j < ncols; ++j)
+                outfile << raster(i, j) << " ";
+            outfile << '\n';
+        }
+
+        outfile.close(); // close the file
+    }
+   
+}
+
+
 int main(int argc, const char* argv[])
 {
     
-    Raster r(3, 3);
-    r.fill();
-    r(2, 2) = 1;
+    //Raster r(3, 3);
+    //r.fill();
+    //r(2, 2) = 1;
     //std::cout<<r(0,0);
 
-    RasterCell cella(0, 0, 20, 1), cellb(0, 0, 30, 2);
-    std::cout << (cella < cellb);
+    //RasterCell cella(0, 0, 20, 1), cellb(0, 0, 30, 2);
+    //std::cout << (cella < cellb);
 
-    //GDALDataset* input_dataset = nullptr;
- //   GDALAllRegister();
- //   input_dataset = (GDALDataset*)GDALOpen("D:/AlbertQ2/GEO1015/N25W101.hgt", GA_ReadOnly); // a nice tile I used for testing
- //   if (input_dataset == nullptr) {
- //       std::cerr << "Couldn't open file" << std::endl;
- //       return 1;
- //   }
+    GDALDataset* input_dataset = nullptr;
+    GDALAllRegister();
+    input_dataset = (GDALDataset*)GDALOpen("D:/AlbertQ2/GEO1015/N25W101.hgt", GA_ReadOnly); // a nice tile I used for testing
+    if (input_dataset == nullptr) {
+        std::cerr << "Couldn't open file" << std::endl;
+        return 1;
+    }
 
     //Print dataset info
-    /*double geo_transform[6];
-    std::cout << "Driver: " << input_dataset->GetDriver()->GetDescription() << "/" << input_dataset->GetDriver()->GetMetadataItem(GDAL_DMD_LONGNAME) << '\n';
-    std::cout << "Size is " << input_dataset->GetRasterXSize() << "x" << input_dataset->GetRasterYSize() << "x" << input_dataset->GetRasterCount() << '\n';
+    std::cout << '\n';
+    std::cout << "dataset info: " << '\n';
+
+    double geo_transform[6]{ 0 };
+    std::cout << "Driver: " << input_dataset->GetDriver()->GetDescription() 
+        << "/" << input_dataset->GetDriver()->GetMetadataItem(GDAL_DMD_LONGNAME) << '\n';
+    std::cout << "width size is: " << input_dataset->GetRasterXSize() << '\n';
+    std::cout << "hight size is: " << input_dataset->GetRasterYSize() << '\n';
+    std::cout << "the number of raster bands: " << input_dataset->GetRasterCount() << '\n';
+    
     if (input_dataset->GetProjectionRef() != NULL) std::cout << "Projection is '" << input_dataset->GetProjectionRef() << "'" << '\n';
     if (input_dataset->GetGeoTransform(geo_transform) == CE_None) {
         std::cout << "Origin = (" << geo_transform[0] << ", " << geo_transform[3] << ")" << '\n';
         std::cout << "Pixel Size = (" << geo_transform[1] << ", " << geo_transform[5] << ")" << '\n';
-    }*/
+    }//Origin is the top-left corner
 
     // Print Band 1 info
-    /*GDALRasterBand* input_band = nullptr;
+    std::cout << '\n';
+    std::cout << "Band 1 info: " << '\n';
+
+    GDALRasterBand* input_band = nullptr;
     int nBlockXSize, nBlockYSize;
     int bGotMin, bGotMax;
-    double adfMinMax[2];
+    double adfMinMax[2]{ 0 };
     input_band = input_dataset->GetRasterBand(1);
-    input_band->GetBlockSize(&nBlockXSize, &nBlockYSize);
-    std::cout << "Band 1 Block=" << nBlockXSize << "x" << nBlockYSize << " Type=" << GDALGetDataTypeName(input_band->GetRasterDataType()) << " ColorInterp=" << GDALGetColorInterpretationName(input_band->GetColorInterpretation()) << '\n';
+    input_band->GetBlockSize(&nBlockXSize, &nBlockYSize); //XSize=3601, YSize=1, "scanline"
+    std::cout << "Band 1 Block=" << nBlockXSize << " x " << nBlockYSize << " Type=" 
+        << GDALGetDataTypeName(input_band->GetRasterDataType()) << " ColorInterp=" 
+        << GDALGetColorInterpretationName(input_band->GetColorInterpretation()) << '\n';
+
     adfMinMax[0] = input_band->GetMinimum(&bGotMin);
     adfMinMax[1] = input_band->GetMaximum(&bGotMax);
     if (!(bGotMin && bGotMax)) GDALComputeRasterMinMax((GDALRasterBandH)input_band, TRUE, adfMinMax);
-    std::cout << "Min=" << adfMinMax[0] << " Max=" << adfMinMax[1] << '\n';*/
+    std::cout << "Min=" << adfMinMax[0] << " Max=" << adfMinMax[1] << '\n';
 
     // Read Band 1 line by line
-    /*int nXSize = input_band->GetXSize();
-    int nYSize = input_band->GetYSize();
+    int nXSize = input_band->GetXSize(); //width
+    int nYSize = input_band->GetYSize(); //height
     Raster input_raster(nXSize, nYSize);
-    for (int current_scanline = 0; current_scanline < nYSize; ++current_scanline) {
-        int* scanline = (int*)CPLMalloc(sizeof(float) * nXSize);
+
+    for (int current_scanline = 0; current_scanline < nYSize; ++current_scanline)
+    {
+        int* scanline = (int*)CPLMalloc(sizeof(float) * nXSize); // DONT forget to use CPLFree(scanline)
         if (input_band->RasterIO(GF_Read, 0, current_scanline, nXSize, 1,
             scanline, nXSize, 1, GDT_Int32,
-            0, 0) != CPLE_None) {
+            0, 0) != CPLE_None)
+        {
             std::cerr << "Couldn't read scanline " << current_scanline << std::endl;
             return 1;
-        } input_raster.add_scanline(scanline);
-        CPLFree(scanline);
-    } std::cout << "Created raster: " << input_raster.max_x << "x" << input_raster.pixels.size() / input_raster.max_y << " = " << input_raster.pixels.size() << '\n';
+        }
+        input_raster.add_scanline(scanline);
+        CPLFree(scanline); //corresponding to the allocation "scanline"
+    }
 
-    GDALClose(input_dataset);*/
-	/*std::cout << "hello"<<'\n';
-    std::cout << "world" << '\n';*/
+    std::cout << "Created raster: " << input_raster.max_x << " x " 
+        << input_raster.max_y << " = " << input_raster.pixels.size() << '\n';
+    
+    double yll = 25;
+    output_raster(input_raster, geo_transform[1], geo_transform[0], geo_transform[3]);
+    
+    // Flow direction
+    //Raster flow_direction(input_raster.max_x, input_raster.max_y);
+    //flow_direction.fill();
+    //std::priority_queue<RasterCell, std::deque<RasterCell>> cells_to_process_flow;
+    // to do
+
+    // Write flow direction
+    // to do
+
+    // Flow accumulation
+    //Raster flow_accumulation(input_raster.max_x, input_raster.max_y);
+    // to do
+
+    // Write flow accumulation
+    // to do
+
+    // Close input dataset
+    GDALClose(input_dataset);
+
 	return 0;
 }
